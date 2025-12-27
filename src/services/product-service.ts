1f0dc8ef49f6837692c78674272c60452d73bbb1
jonaferreir4 <jonaferreira2204@gmail.com>
2025-12-27 01:05:28 -0300

    feat: create get product by id

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -6,11 +6,18 @@ export const productService = () => {
         return MOCK_PRODUCTS;
     }
 
+    
+    async function getProductById(id: string | number) {
+        await new Promise(r => setTimeout(r, 500));
+        const product = MOCK_PRODUCTS.find(p => p.id === Number(id));
+        return product || null;
+    }
+
     async function createProduct( data: any) {
         await new Promise(r => setTimeout(r, 600));
         console.log("API: Produto cadastrado", data);
         return { success: true, data };
     };
 
-    return { createProduct, getProducts };
+    return { createProduct, getProducts, getProductById };
 }
